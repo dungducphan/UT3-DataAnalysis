b3390b17717c33f3639da4b3a9fe023f6bd5b7df
Dung D. Phan <brianp.dung@utexas.edu>
2025-02-05 13:14:08 -0600

    Update code and results on picoscope calib

--- PicoScope_Recalib/README.md
+++ PicoScope_Recalib/README.md
@@ -6,3 +6,8 @@ Shot log: https://utexas.app.box.com/file/1749792525115
     - 50 shots for LeCroy: 75-123 (missing 1 shot)
     - 50 shots for PicoScope: 134-183
 
+Hard coded in the PicoScope file reader that the first 500 data points (contains only pedestal) are removed.
+
+Integrate Domain:
+    - LeCroy: 1751-end
+    - PicoScope: 1202-end
--- PicoScope_Recalib/main.cpp
+++ PicoScope_Recalib/main.cpp
@@ -38,6 +38,7 @@ waveform ReadSingleWaveformLeCroy(const std::string& filename) {
   file >> line;
 
   // Read the data lines
+  int counter = 0;
   while (file >> line) {
     std::istringstream ss(line);
     double x_val, y_val;
@@ -45,9 +46,11 @@ waveform ReadSingleWaveformLeCroy(const std::string& filename) {
     if (std::getline(ss, x_str, ',') && std::getline(ss, y_str, ',')) {
       double x_val = std::stod(x_str);
       double y_val = std::stod(y_str);
+      // if (x_val > 0.5E-6 && x_val < 0.52E-6) std::cout << "lecroy: " << counter << std::endl;
       x.push_back(x_val);
       y.push_back(y_val);
     }
+    counter++;
   }
 
   file.close();
@@ -73,18 +76,18 @@ waveform ReadSingleWaveformPicoScope(const std::string& filename) {
   file >> line;
 
   // Read the data lines
+  int counter = 0;
   while (file >> line) {
-    std::cout << line << std::endl;
     std::istringstream ss(line);
     std::string x_str, y_str;
     if (std::getline(ss, x_str, ',') && std::getline(ss, y_str, ',')) {
       double x_val = std::stod(x_str);
       double y_val = std::stod(y_str);
-      if (x_val > 500) {
-        x.push_back(x_val * 1E-9); // ns
-        y.push_back(y_val * 1E-3); // mV
-      }
+      // if (x_val > 500 && x_val < 510) std::cout << "picoscope: " << counter << std::endl;
+      x.push_back(x_val * 1E-9); // ns
+      y.push_back(y_val * 1E-3); // mV
     }
+    counter++;
   }
 
   file.close();
@@ -105,17 +108,119 @@ TGraph* WaveformToTGraph(const waveform& wf) {
   return gr;
 }
 
-int main() {
-  std::string filename = "C2--20250114--00120.txt";
-  auto wf_01 = ReadSingleWaveformLeCroy(LeCroyDataDir + filename);
-  // std::string filename = "20250114_01.csv";
-  // auto wf_01 = ReadSingleWaveformPicoScope(PicoScopeDataDir + filename);
-  auto gr_01 = WaveformToTGraph(wf_01);
+void BackgroundSubtraction(TGraph* gr, int idx) {
+  double mean = 0;
+  for (int i = 0; i < idx; i++) {
+    double x, y;
+    gr->GetPoint(i, x, y);
+    mean += y;
+  }
+  mean = mean / idx;
 
-  auto c = new TCanvas("c", "c", 800, 600);
+  for (int i = 0; i < gr->GetN(); i++) {
+    double x, y;
+    gr->GetPoint(i, x, y);
+    gr->SetPoint(i, x, y - mean);
+  }
+}
+
+void PlotLeCroy(bool bkgdSub = false, int idx = 1000) {
+  int shotID = 120;
+
+  for (shotID = 75; shotID < 124; shotID++) {
+    std::string filename = Form("C2--20250114--%05i.txt", shotID);
+    auto wf_01 = ReadSingleWaveformLeCroy(LeCroyDataDir + filename);
+    auto gr_01 = WaveformToTGraph(wf_01);
+    if (bkgdSub) BackgroundSubtraction(gr_01, idx);
+
+    auto c = new TCanvas("c", "c", 800, 600);
+    gStyle->SetOptStat(0);
+    gr_01->Draw("APL");
+    c->SaveAs(Form("test/lecroy/test_%05i.png", shotID));
+
+    delete gr_01;
+    delete c;
+  }
+}
+
+void PlotPicoScope(bool bkgdSub = false, int idx = 1000) {
+  int shotID = 0;
+
+  for (shotID = 1; shotID < 53; shotID++) {
+    std::string filename = Form("20250114_%02i.csv", shotID);
+    auto wf_01 = ReadSingleWaveformPicoScope(PicoScopeDataDir + filename);
+    auto gr_01 = WaveformToTGraph(wf_01);
+    if (bkgdSub) BackgroundSubtraction(gr_01, idx);
+
+    auto c = new TCanvas("c", "c", 800, 600);
+    gStyle->SetOptStat(0);
+    gr_01->Draw("APL");
+    c->SaveAs(Form("test/picoscope/test_%05i.png", shotID));
+
+    delete gr_01;
+    delete c;
+  }
+}
+
+double IntegrateTGraph(TGraph* gr, int idx_min) {
+  return gr->Integral(idx_min, -1);
+}
+
+int main() {
+  // PlotLeCroy(true, 1750);
+  // PlotPicoScope(true, 1200);
+
+  int integralMarkerLeCroy = 1752;
+  int integralMarkerPicoScope = 1202;
+  int nbins = 50;
+  int min_charge = 0;
+  int max_charge = 20;
+
+  auto h_LeCroy = new TH1D("h_LeCroy", "h_LeCroy", nbins, min_charge, max_charge);
+  for (int shotID = 75; shotID < 124; shotID++) {
+    std::string filename = Form("C2--20250114--%05i.txt", shotID);
+    auto wf_01 = ReadSingleWaveformLeCroy(LeCroyDataDir + filename);
+    auto gr_01 = WaveformToTGraph(wf_01);
+    BackgroundSubtraction(gr_01, integralMarkerLeCroy - 1);
+    double integral = IntegrateTGraph(gr_01, integralMarkerLeCroy);
+    h_LeCroy->Fill(integral * 1E12 / 50);
+    std::cout << "LeCroy: " << shotID << " " << integral * 1E9 << std::endl;
+    delete gr_01;
+  }
+
+  auto h_PicoScope = new TH1D("h_PicoScope", "h_PicoScope", nbins, min_charge, max_charge);
+  for (int shotID = 1; shotID < 53; shotID++) {
+    std::string filename = Form("20250114_%02i.csv", shotID);
+    auto wf_01 = ReadSingleWaveformPicoScope(PicoScopeDataDir + filename);
+    auto gr_01 = WaveformToTGraph(wf_01);
+    BackgroundSubtraction(gr_01, integralMarkerPicoScope - 1);
+    double integral = IntegrateTGraph(gr_01, integralMarkerPicoScope);
+    h_PicoScope->Fill(integral * 1E12 / 50);
+    std::cout << "PicoScope: " << shotID << " " << integral * 1E9 << std::endl;
+    delete gr_01;
+  }
+
   gStyle->SetOptStat(0);
-  gr_01->Draw("APL");
-  c->SaveAs("test.png");
+  auto c = new TCanvas("c", "c", 800, 800);
+  h_LeCroy->SetLineColor(kRed);
+  h_LeCroy->SetFillColorAlpha(kRed, 0.5);
+  h_LeCroy->SetTitle("Data Collected on 2025-01-14");
+  h_LeCroy->GetXaxis()->SetTitle("Charge (pC)");
+  h_LeCroy->GetYaxis()->SetTitle("Counts");
+  h_LeCroy->GetXaxis()->CenterTitle();
+  h_LeCroy->GetYaxis()->CenterTitle();
+  h_LeCroy->Draw();
+  h_PicoScope->SetLineColor(kBlue);
+  h_PicoScope->SetFillColorAlpha(kBlue, 0.5);
+  h_PicoScope->Draw("SAME");
+
+  auto leg = new TLegend(0.5, 0.5, 0.85, 0.7);
+  leg->AddEntry(h_LeCroy, Form("LeCroy: %1.2f pC", h_LeCroy->GetMean()), "f");
+  leg->AddEntry(h_PicoScope, Form("PicoScope: %1.2f pC", h_PicoScope->GetMean()), "f");
+  leg->SetBorderSize(0);
+  leg->Draw();
+
+  c->SaveAs("test/hist.png");
 
   return 0;
 }
