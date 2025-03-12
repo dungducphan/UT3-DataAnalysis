08342c419c2dee1ddde5476921ebb5344a8beaa3
Dung D. Phan <brianp.dung@utexas.edu>
2025-03-12 11:06:48 -0500

    Fix some issues with the align.C, new data

--- AstranisDataAnalysis/CollectorVoltage/align.C
+++ AstranisDataAnalysis/CollectorVoltage/align.C
@@ -20,8 +20,8 @@ std::vector<std::string> getAllTxtFiles(const char *folderPath) {
 
     while ((entry = readdir(dp))) {
         if (entry->d_type == DT_REG) {
-            const char *ext = strrchr(entry->d_name  '.');
-            if (ext && strcmp(ext  ".txt") == 0) {
+            const char *ext = strrchr(entry->d_name, '.');
+            if (ext && strcmp(ext, ".txt") == 0) {
                 txtFiles.push_back(entry->d_name);
             }
         }
@@ -32,11 +32,11 @@ std::vector<std::string> getAllTxtFiles(const char *folderPath) {
 }
 
 void alignDate(std::string date) {
-    const char *folderPath = Form("/home/dphan/Documents/GitHub/UT3-DataAnalysis/AstranisDataAnalysis/CollectorVoltage/%s"  date.c_str());
+    const char *folderPath = Form("/home/dphan/Documents/GitHub/UT3-DataAnalysis/AstranisDataAnalysis/CollectorVoltage/%s", date.c_str());
     auto list = getAllTxtFiles(folderPath);
 
-    std::map<int  std::string> sequenceNumberToTimestamp;
-    std::map<int  float> sequenceNumberToVoltage;
+    std::map<int, std::string> sequenceNumberToTimestamp;
+    std::map<int, float> sequenceNumberToVoltage;
     for (const auto &fileName : list) {
         std::string filePath = std::string(folderPath) + "/" + fileName;
         std::ifstream file(filePath);
@@ -46,7 +46,7 @@ void alignDate(std::string date) {
         }
 
         std::string line;
-        for (int i = 0; i < 4 && std::getline(file  line); ++i) {
+        for (int i = 0; i < 4 && std::getline(file, line); ++i) {
             // Read lines until the fourth line
         }
 
@@ -54,10 +54,10 @@ void alignDate(std::string date) {
             std::istringstream iss(line);
             std::string token;
             int tokenIndex = 0;
-            while (std::getline(iss  token  ' ')) {
+            while (std::getline(iss, token, ',')) {
                 if (tokenIndex == 1) {
                     std::string timestamp = token.substr(12); // Extract the time part
-                    std::string sequenceNumber = fileName.substr(14  5); // Extract the sequence number
+                    std::string sequenceNumber = fileName.substr(14, 5); // Extract the sequence number
                     sequenceNumberToTimestamp[std::stoi(sequenceNumber)] = timestamp;
                     break;
                 }
@@ -66,38 +66,41 @@ void alignDate(std::string date) {
         }
 
         float minVoltage = 9E9;
-        std::getline(file  line);
-        while (std::getline(file  line)) {
+        std::getline(file, line);
+        while (std::getline(file, line)) {
             std::istringstream iss(line);
-            std::string firstFloat  secondFloat;
-            std::getline(iss  firstFloat  ' ');
-            std::getline(iss  secondFloat  ' ');
+            std::string firstFloat, secondFloat;
+            std::getline(iss, firstFloat, ',');
+            std::getline(iss, secondFloat, ',');
             float secondNumber = std::stod(secondFloat);
-            minVoltage = std::min(minVoltage  secondNumber);
+            minVoltage = std::min(minVoltage, secondNumber);
         }
-        sequenceNumberToVoltage[std::stoi(fileName.substr(14  5))] = minVoltage;
+        sequenceNumberToVoltage[std::stoi(fileName.substr(14, 5))] = minVoltage;
 
         file.close();
     }
 
-    std::ofstream csvFile(Form("%s.csv"  date.c_str()));
+    std::ofstream csvFile(Form("%s.csv", date.c_str()));
     if (!csvFile.is_open()) {
         perror("ofstream");
         return;
     }
 
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 2000; i++) {
         if (sequenceNumberToTimestamp.find(i) == sequenceNumberToTimestamp.end()) {
             continue;
         }
-        csvFile << i << " " << sequenceNumberToTimestamp[i] << " " << sequenceNumberToVoltage[i] << "\n";
+        csvFile << i << "," << sequenceNumberToTimestamp[i] << "," << sequenceNumberToVoltage[i] << "\n";
     }
 
     csvFile.close();
 }
 
 void align() {
-    alignDate("20250303");
+    alignDate("20250311");
+    // alignDate("20250310");
+    // alignDate("20250304");
+    // alignDate("20250303");
     // alignDate("20250227");
     // alignDate("20250226");
     // alignDate("20250225");
